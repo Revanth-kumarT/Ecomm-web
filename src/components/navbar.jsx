9e0e0bd0a124de367b544939b3aaf8026a320505
Revanth-kumarT <revanthkumar8686@gmail.com>
2024-07-30 10:45:03 +0530

    nikhl commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,6 +16,7 @@ export const Navbar = () => {
         <div className="test">
           <h1>revanth test</h1>
           <h1>likith git test2</h1>
+          <h1>nikhil git test2</h1>
         </div>
       </div>
     </div>
